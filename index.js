1b522eca65ea980e553798870f325b20ebabdb07
vamshi <vamshis2017@gmail.com>
2022-10-22 14:04:35 +0530

    added new func to nav-bar

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 let USER_DB = []
-const signUp = () =>{
+const signUp = () => {
     let firstName = document.getElementById('firstName').value
     let lastName = document.getElementById('lastName').value
     let phoneNumber = document.getElementById('phone-number').value
     let email = document.getElementById('email').value
     let password = document.getElementById('password').value
+
+    document.getElementById('sign-up-form').reset()
 
     let userDetails = {
         firstName,
@@ -16,50 +18,62 @@ const signUp = () =>{
     };
 
     USER_DB.push(userDetails)
-    document.getElementById('sign-up-form').reset()
-    console.log(USER_D)
+
     alert('Sign Up Successfull!')
 
     changeNavLinks(userDetails)
-} 
+}
 
-const signIn = () =>{
+const signIn = () => {
     let enteredEmail = document.getElementById('login-email').value
     let enteredPassword = document.getElementById('login-password').value
 
-    alert(USER_DB.find(user => user.email === enteredEmail && user.password === enteredPassword)? 'Access Granted':'Access Denied')
+    let requiredUser = USER_DB.find(
+        (user) => 
+            user.email === enteredEmail && user.password === enteredPassword
+    );
+    if (requiredUser) {
+        alert('Access Granted')
+        changeNavLinks(requiredUser)
+    } else {
+        alert('Access Denied')
+    }
     document.getElementById('sign-in-form').reset()
 }
 
-const goToSignUp = () =>{
+const goToSignUp = () => {
     document.getElementById('home').style.display = 'none'
     document.getElementById('sign-up').style.display = 'block'
     document.getElementById('sign-in').style.display = 'none'
 }
 
-const goToHome = () =>{
+const goToHome = () => {
     document.getElementById('sign-up').style.display = 'none'
     document.getElementById('sign-in').style.display = 'none'
     document.getElementById('home').style.display = 'block'
 }
 
-const goToSignIn = () =>{
+const goToSignIn = () => {
     document.getElementById('home').style.display = 'none'
     document.getElementById('sign-in').style.display = 'block'
     document.getElementById('sign-up').style.display = 'none'
 }
 
- const changeNavLinks = (currentUser) => {
+const changeNavLinks = (currentUser) => {
     let { firstName, lastName } = currentUser
     document.getElementById('sign-up-nav-link').style.display = 'none'
     document.getElementById('sign-in-nav-link').style.display = 'none'
-
-    document.getElementById('profile-nav-link').style.display = 'none'
-    document.getElementById('sign-out-nav-link').style.display = 'none'
+    document.getElementById('profile-nav-link').style.display = 'block'
+    document.getElementById('sign-out-nav-link').style.display = 'block'
 
     document.getElementById('profile-nav-link').innerText = `Hi, ${firstName} ${lastName}`
- }
+}
 
- const signOut = () => {
+const signOut = () => {
+    document.getElementById('profile-nav-link').innerText = ''
 
- }
+    document.getElementById('sign-up-nav-link').style.display = 'block'
+    document.getElementById('sign-in-nav-link').style.display = 'block'
+    document.getElementById('profile-nav-link').style.display = 'none'
+    document.getElementById('sign-out-nav-link').style.display = 'none'
+}
