b61d5cfed5e3be838135e09700f4d8952f50d35f
vamshi <vamshis2017@gmail.com>
2022-10-23 23:58:07 +0530

    added new things

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 
 let USER_DB = []
+
+let encryptionRule = {
+    'A': 'N', 'B': 'O', 'C': 'P', 'D': 'Q',
+	'E': 'R', 'F': 'S', 'G': 'T', 'H': 'U',
+	'I': 'V', 'J': 'W', 'K': 'X', 'L': 'Y',
+	'M': 'Z', 'N': 'A', 'O': 'B', 'P': 'C',
+	'Q': 'D', 'R': 'E', 'S': 'F', 'T': 'G',
+	'U': 'H', 'V': 'I', 'W': 'J', 'X': 'K',
+	'Y': 'L', 'Z': 'M',
+	'a': 'n', 'b': 'o', 'c': 'p', 'd': 'q',
+	'e': 'r', 'f': 's', 'g': 't', 'h': 'u',
+	'i': 'v', 'j': 'w', 'k': 'x', 'l': 'y',
+	'm': 'z', 'n': 'a', 'o': 'b', 'p': 'c',
+	'q': 'd', 'r': 'e', 's': 'f', 't': 'g',
+	'u': 'h', 'v': 'i', 'w': 'j', 'x': 'k',
+	'y': 'l', 'z': 'm',
+	'0': '5', '1': '6', '2': '7', '3': '8',
+	'4': '9', '5': '0', '6': '1', '7': '2',
+	'8': '3', '9': '4',
+	'!': '#', '$': '%', '&': '+', '-': '@',
+	'_': '~', '#': '!', '%': '$', '+': '&',
+	'@': '-', '~': '_'
+}
+
+const encrypt = (inputString) => {
+    let encryptedString = ''
+    for (let char of inputString) {
+        encryptedString = encryptedString + encryptionRule[char]
+    }
+    return encryptedString
+}
+
 const signUp = () => {
     let firstName = document.getElementById('firstName').value
     let lastName = document.getElementById('lastName').value
     let phoneNumber = document.getElementById('phone-number').value
     let email = document.getElementById('email').value
     let password = document.getElementById('password').value
+
+    let encryptedPassword = encrypt(password)
 
     document.getElementById('sign-up-form').reset()
 
@@ -14,7 +48,7 @@ const signUp = () => {
         lastName,
         phoneNumber,
         email,
-        password
+        password: encryptedPassword
     };
 
     USER_DB.push(userDetails)
